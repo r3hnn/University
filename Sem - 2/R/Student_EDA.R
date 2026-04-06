a6e1b91de353329b2ef6586f17c81aa10ebbf01a
Rehaan <md.rehaan.shaikh2@gmail.com>
2026-04-06 18:24:39 +0530

    df_copy <- df # Create a copy of the original data frame for cleaning pie(table(df_copy$gender) removed because it was not needed for the cleaning process

--- Sem - 2/R/Student_EDA.R
+++ Sem - 2/R/Student_EDA.R
@@ -1,6 +1,7 @@
 # 1. Import the dataset
 # Put your CSV file in the same folder as this script
 df <- read.csv("C:\\Users\\rehan\\Downloads\\archive\\StudentsPerformance.csv")
+df_copy <- df
 
 # 2. Understand structure
 str(df)
@@ -39,42 +40,38 @@ pie(table(df$gender),
 # 5. Data Cleaning
 # Handle missing values (Mean imputation)
 # This replaces NAs with the average math score
-df$math.score[is.na(df$math.score)] <- mean(df$math.score, na.rm = TRUE)
+df$math.score[is.na(df_copy$math.score)] <- mean(df$math.score, na.rm = TRUE)
 
 # Detect duplicates
-sum(duplicated(df))
+sum(duplicated(df_copy))
 
 # Identify outliers
-boxplot.stats(df$math.score)$out
+boxplot.stats(df_copy$math.score)$out
 
 # Basic statistics
-mean(df$math.score)
-median(df$math.score)
-sd(df$math.score)
+mean(df_copy$math.score)
+median(df_copy$math.score)
+sd(df_copy$math.score)
 
 # 6. Visualizations (AFTER)
 # We just run them again to show the cleaned data
-hist(df$math.score,
+hist(df_copy$math.score,
     col = "blue",
     main = "Distribution of Math Scores (After Cleaning)",
     xlab = "Math Score (Marks out of 100)",
     ylab = "Number of Students")
-barplot(table(df$parental.level.of.education),
+barplot(table(df_copy$parental.level.of.education),
     col="red",
     main = "Distribution of Parental Levels of Education (After Cleaning)",
     xlab = "Parental Level of Education (Highest Degree)",
     ylab = "Number of Students")
-boxplot(df$reading.score,
+boxplot(df_copy$reading.score,
     col="green",
     main = "Distribution of Reading Scores (After Cleaning)",
     xlab = "Reading Score (Marks out of 100)",
     ylab = "Number of Students")
-plot(df$math.score, df$writing.score,
+plot(df_copy$math.score, df_copy$writing.score,
     col="purple",
     main = "Math vs Writing Scores (After Cleaning)",
     xlab = "Math Score (Marks out of 100)",
     ylab = "Writing Score")
-pie(table(df$gender),
-    col=c("pink", "lightblue"),
-    main = "Distribution of Gender (After Cleaning)",
-    labels = c("Female", "Male"))
