bc006e1ea236096bd41c29230ac0db6385255fdb
rehaan <md.rehaan.shaikh2@gmail.com>
2026-04-29 19:34:36 +0530

    Program 6 and 7 Added

---
+++ Sem - 2/R/Program 6.r
@@ -0,0 +1,32 @@
+student_data <- data.frame(
+    student = c("A","B","C","D","E","F","G","H"),
+    study_hours = c(2, 3, 4, 5, 6, 3, 7, 8),
+    Marks = c(50, 55, 60, 65, 70, 55, 75, 80),
+    Attendence = c(75, 80, 85, 90, 95, 82, 88, 92)
+)
+
+print(student_data)
+
+library(ggplot2)
+
+ggplot(student_data, aes(x = study_hours, y = Marks)) +
+    geom_bar(stat = "identity", fill = "blue") +
+    labs(title = "Marks of Students",
+        x = "Student",
+        y = "Marks") +
+    theme_minimal()
+
+ggplot(student_data, aes(x = study_hours, y = Marks))+
+    geom_point(color = "red", size = 3) +
+    geom_line(color = "green", size = 1)
+    labs(title = "Study Hours VS Marks",
+        x = "Study Hours",
+        y = "Marks") +
+    theme_classic()
+
+ggplot(student_data, aes(x = Attendence)) +
+    geom_histogram(binwidth = 5, fill = "orange", color = "black") +
+    labs(title = "Attendace Distibution",
+    x = "Attendance (%)",
+    y = "Frequency") +
+    theme_light()
---
+++ Sem - 2/R/Program 7.r
@@ -0,0 +1,34 @@
+#Using the ggplot2 package in Rm write progras to create the following visualization:
+#Line Graph to display the trend of sales across different months.
+#Box plot to visualize the distribution of profit across different regions.
+#Density plot to show the distribution of sales values.
+library(ggplot2)
+sales_data <- data.frame(
+    Month = c("Jan", "Feb", "Mar", "Apr", "May", "Jun"),
+    Sales = c(200, 250, 300, 280, 320, 350),
+    Profit = c(50, 60, 80, 70, 90, 100),
+    Region = c("North", "South", "East", "West", "North", "South")
+)
+
+print(sales_data)
+
+ggplot(sales_data, aes(x = Month, y = Sales)) +
+    geom_line(color = "steelblue", size = 1, group = 1) +
+    labs(title = "Monthly Sales Trend",
+        x = "Month",
+        y = "Sale") +
+    theme_light()
+
+ggplot(sales_data, aes(x = Region, y = Profit, fill = Region)) +
+    geom_boxplot() + 
+    labs(title = "Profit Distribution by Region",
+        x = "Region",
+        y = "Profit") +
+    theme_classic()
+
+ggplot(sales_data, aes(x = Sales)) +
+    geom_density(fill = "green", alpha = 0.8) +
+    labs(title = "Density plot for Sales",
+        x = "Sales",
+        y = "Density") +
+    theme_classic()
