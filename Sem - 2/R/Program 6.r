2a5bbf5043d13bee9aac055cdf3ec51f4d058814
rehaan <md.rehaan.shaikh2@gmail.com>
2026-05-05 13:57:25 +0530

    Fix typo in title for attendance distribution in Program 6

--- Sem - 2/R/Program 6.r
+++ Sem - 2/R/Program 6.r
@@ -22,7 +22,7 @@ ggplot(student_data, aes(x = student, y = Marks)) +
 
 ggplot(student_data, aes(x = study_hours, y = Marks))+
     geom_point(color = "red", size = 3) +
-    geom_line(color = "green", size = 1)
+    geom_line(color = "green", size = 1) +
     labs(title = "Study Hours VS Marks",
         x = "Study Hours",
         y = "Marks") +
@@ -30,7 +30,7 @@ ggplot(student_data, aes(x = study_hours, y = Marks))+
 
 ggplot(student_data, aes(x = Attendence)) +
     geom_histogram(binwidth = 5, fill = "orange", color = "black") +
-    labs(title = "Attendace Distibution",
+    labs(title = "Attendance Distribution",
     x = "Attendance (%)",
     y = "Frequency") +
     theme_light()
